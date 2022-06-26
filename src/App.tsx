92d05636b2b288cdffe8e64bf8e500830cc442bf
AbdulRahman <61910010+Abdul1810@users.noreply.github.com>
2022-06-26 16:59:21 +0530

    trailing speed Check

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ export default function App() {
       <AnimatedCursor
           innerSize={40}
           outerSize={0}
-          trailingSpeed={10}
+          trailingSpeed={15}
           color='255, 255, 255'
           innerScale={1.8}
           clickables={[
